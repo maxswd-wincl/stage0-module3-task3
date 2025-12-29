a8a8921f2e137192e11af4f480883c6bad5d6110
maxswd-wincl <max@wincl.io>
2025-12-29 11:17:14 +0500

    Implement variable reassignment example
    Added code to demonstrate variable reassignment and linking.

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,32 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+   // initial variables
+        int first = 1;
+        int second = 10;
+        int third = 100;
+
+        // print initial values
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        // create "links" (copies of values)
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        // reassign original variables
+        first = 15;
+        second = 6;
+        third = 4;
+
+        // print all variables in order of declaration
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
     }
 }
